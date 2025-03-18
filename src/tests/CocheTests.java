ab39085a48a521046086981f2627c602d767c6fa
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2025-03-18 05:55:20 +0100

    creada pila de tests unitarios

--- src/tests/CocheTests.java
+++ src/tests/CocheTests.java
@@ -2,13 +2,17 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CocheTests {
     private static Coche coche;
+    private static Coche coche2;
 
     @BeforeAll
     public static void crearCoche() {
         coche = new Coche();
+        coche2 = new Coche();
     }
 
     @Test
@@ -25,5 +29,17 @@ public class CocheTests {
         // 10 - 5
         coche.frenar(5);
         Assertions.assertEquals(5, coche.getVelocidade());
+    }
+
+    @ParameterizedTest(name = "Acelerar de {0} a {1}")
+    @CsvSource({
+            "15, 15",
+            "15, 30",
+            "10, 40",
+            "100, 140"
+    })
+    public void acelerarVariasVeces(int first, int expectedResult) {
+        coche2.acelerar(first);
+        Assertions.assertEquals(coche2.getVelocidade(), expectedResult);
     }
 }

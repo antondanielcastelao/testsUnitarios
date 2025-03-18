7fe590edc84c3068c8cab85359d6c5d4dd19cf7c
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2025-03-18 05:43:46 +0100

    ejercicio de prog

---
+++ src/Coche.java
@@ -0,0 +1,19 @@
+public class Coche {
+    private int velocidade ;
+
+    public Coche(){
+        velocidade =0 ;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void acelerar (int valor) {
+        velocidade += valor;
+    }
+
+    public void frenar (int menos) {
+        velocidade -= menos;
+    }
+}
